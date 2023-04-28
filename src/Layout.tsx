697befebc936e72714ef7f24809bab362fda13c0
Declan Lawson <109486463+declanl2@users.noreply.github.com>
2023-04-28 14:53:30 +1000

    Funny messages for 404 page.

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent, ReactElement } from 'react'
 import { Outlet } from 'react-router-dom'
-import { Flex, Box } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 
 import { NavBar } from './components/NavBar'
 
@@ -8,7 +8,6 @@ export const Layout: FunctionComponent = (): ReactElement => {
 	return (
 		<Flex minH="100vh" flexDirection="column">
 			<NavBar />
-
 			{/* An <Outlet> renders whatever child route is currently active,
 			so you can think about this as a placeholder for the child routes
 			we defined in index.tsx */}
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,29 @@
 import { FunctionComponent, ReactElement } from 'react'
 import { Text, Flex, Box, Image } from '@chakra-ui/react'
 
+const msgs: string[] = [
+	'Whoops! Looks like we took a wrong turn at Albuquerque.',
+	"This isn't the page you're looking for. Move along.",
+	"I'm sorry, Dave. I'm afraid I can't find that page.",
+	"Uh-oh, looks like we've got a 404 on our hands. Better call in the tech support cavalry!",
+	'Well, this is awkward... We seem to have misplaced the page you were looking for.',
+	"The page you're looking for is in another castle. Sorry about that.",
+	"It's not you, it's me. I can't seem to find the page you're looking for.",
+	"Don't worry, be 404.",
+	"Looks like the page you were searching for has gone into hiding. Maybe it's in the witness protection program?",
+	"The good news: You're not lost. The bad news: The page you were looking for is.",
+]
+
+function randomMessage() {
+	const min = 0
+	const max: number = msgs.length - 1
+	const randomNum: number = Math.floor(Math.random() * (max - min) + min)
+	return msgs[randomNum]
+}
+
 export const NotFound: FunctionComponent = (): ReactElement => {
+	const msg: string = randomMessage()
+
 	return (
 		<>
 			<Flex
@@ -12,10 +34,11 @@ export const NotFound: FunctionComponent = (): ReactElement => {
 			>
 				<Box />
 				<Flex flexDirection="column" alignItems="center">
-					<Text fontSize="3xl" fontWeight="bold">
+					<Text fontSize="8xl" fontWeight="bold">
 						404
 					</Text>
 					<Text>Page not found.</Text>
+					<Text fontStyle="italic">{msg}</Text>
 					<Box paddingTop="3">
 						<Image
 							shadow="dark-lg"
