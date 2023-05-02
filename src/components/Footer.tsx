4758481f09553c112d73d8f2eefb382635d91fc4
Declan Lawson <109486463+declanl2@users.noreply.github.com>
2023-05-02 14:47:18 +1000

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,20 @@
 import { ReactElement, FunctionComponent } from 'react'
 import PurpleWave from '../assets/purplewave.svg'
-import { Image } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 
 export const Footer: FunctionComponent = (): ReactElement => {
+	const imageURL: string = `url(${PurpleWave})`
 	return (
 		<>
-			<Image src={PurpleWave}></Image>
+			<Flex
+				backgroundImage={imageURL}
+				width="100%"
+				height="100vh"
+				backgroundSize="cover"
+				backgroundRepeat="no-repeat"
+				backgroundPosition="bottom"
+				grow="0"
+			></Flex>
 		</>
 	)
 }
