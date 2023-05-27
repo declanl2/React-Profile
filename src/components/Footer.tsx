e7a23fb275f9a0f0b60007ca0c6cf925290d2dfb
declanl2 <109486463+declanl2@users.noreply.github.com>
2023-05-28 01:03:52 +1000

    Updated footer to show info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,34 @@
 import { ReactElement, FunctionComponent } from 'react'
 import PurpleWave from '../assets/purplewave.svg'
-import { Flex } from '@chakra-ui/react'
+import { ListItem, ListIcon, List, Flex, Link } from '@chakra-ui/react'
+import { EmailIcon } from '@chakra-ui/icons'
+import { AiFillGithub } from 'react-icons/ai'
 
 export const Footer: FunctionComponent = (): ReactElement => {
 	const imageURL: string = `url(${PurpleWave})`
+	const email: string = 'hi@declanlawson.dev'
 	return (
 		<>
 			<Flex
 				backgroundImage={imageURL}
-				width="100%"
-				height="100vh"
 				backgroundSize="cover"
 				backgroundRepeat="no-repeat"
 				backgroundPosition="bottom"
-				grow="0"
-			></Flex>
+				width="100%"
+				height="540px"
+				alignItems="flex-end"
+			>
+				<List p="30px">
+					<ListItem>
+						<ListIcon as={EmailIcon} />
+						<Link href={`mailto:${email}`}>{email}</Link>
+					</ListItem>
+					<ListItem>
+						<ListIcon as={AiFillGithub} />
+						<Link href="https://github.com/declanl2">declanl2</Link>
+					</ListItem>
+				</List>
+			</Flex>
 		</>
 	)
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,18 +3,12 @@ import { Flex, Heading, Container, Box } from '@chakra-ui/react'
 
 export const Home: FunctionComponent = (): ReactElement => {
 	return (
-		<Flex
-			alignItems="center"
-			flexDirection="column"
-			flexGrow="1"
-			justifyContent="space-between"
-		>
+		<Flex alignItems="center" justifyContent="space-between" py="10vh">
 			<Box />
 			<Box>
 				<Heading size="3xl">Welcome</Heading>
 				<Container>Hi, my name is Declan. &#128075;</Container>
 			</Box>
-
 			<Box />
 		</Flex>
 	)
