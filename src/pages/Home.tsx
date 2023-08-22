a3308d12444a6178140c9b7b3ba123bc03e378ef
declanl2 <hi@declanlawson.dev>
2023-08-22 20:41:45 +1000

    Updated home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,36 @@
 import { FunctionComponent, ReactElement } from 'react'
-import { Flex, Heading, Container, Box } from '@chakra-ui/react'
+import { Flex, Heading, Container, Box, Text, Link } from '@chakra-ui/react'
 
 export const Home: FunctionComponent = (): ReactElement => {
 	return (
-		<Flex alignItems="center" justifyContent="space-between" py="10vh">
+		<Flex
+			flexDirection="column"
+			alignItems="center"
+			justifyContent="space-between"
+			py="10vh"
+		>
 			<Box />
 			<Box>
 				<Heading size="3xl">Welcome</Heading>
 				<Container>Hi, my name is Declan. &#128075;</Container>
+			</Box>
+			<Box py="40">
+				<Text fontWeight={'bold'} textAlign={'center'} fontSize={'2xl'}>
+					My website still looks a bit plain at the moment &#128517;,
+					but I&apos;m still working on it. &#128512;
+				</Text>
+				<Text textAlign={'center'}>
+					The 404 page works though, so you can try that out if you
+					want (especially if you like cats).
+					<br />
+					<Text
+						fontSize={'3xl'}
+						fontWeight={'bold'}
+						textDecoration={'underline'}
+					>
+						<Link href="/404">404</Link>
+					</Text>
+				</Text>
 			</Box>
 			<Box />
 		</Flex>
