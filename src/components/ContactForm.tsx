42cb030b75ada5443663f33fd6f597ce1465659d
declanl2 <hi@declanlawson.dev>
2023-09-01 21:23:59 +1000

    Work on form with Formik.

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,14 +1,55 @@
-import { FormControl, FormLabel, Input, Box } from '@chakra-ui/react'
+import {
+	FormControl,
+	FormLabel,
+	FormErrorMessage,
+	Input,
+	Button,
+	useColorMode,
+} from '@chakra-ui/react'
 import { FunctionComponent, ReactElement } from 'react'
-import { Field, Form, Formik } from 'formik'
+import { Formik, Form, Field } from 'formik'
 
 export const ContactForm: FunctionComponent = (): ReactElement => {
+	const { colorMode } = useColorMode()
+
+	function validateName(value) {
+		let error
+		if (!value) {
+			error = 'Name is required'
+		}
+		return error
+	}
+
 	return (
-		<Form>
-			<FormControl isRequired>
-				<FormLabel>First name</FormLabel>
-				<Input type="text" />
-			</FormControl>
-		</Form>
+		<Formik
+			initialValues={{ firstName: '', lastName: '', email: '' }}
+			onSubmit={async (values) => {
+				await new Promise((r) => setTimeout(r, 500))
+				alert(JSON.stringify(values, null, 2))
+			}}
+		>
+			<Form>
+				<Field name="name" validate={validateName}>
+					{({ field, form }) => (
+						<FormControl
+							isInvalid={form.errors.name && form.touched.name}
+						>
+							<FormLabel>First name</FormLabel>
+							<Input {...field} placeholder="name" />
+							<FormErrorMessage>
+								{form.errors.name}
+							</FormErrorMessage>
+						</FormControl>
+					)}
+				</Field>
+				<Button
+					type="submit"
+					colorScheme="purple"
+					color={colorMode === 'light' ? 'white' : 'black'}
+				>
+					Submit
+				</Button>
+			</Form>
+		</Formik>
 	)
 }
